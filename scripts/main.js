1543e86a87a9a95d404b278721319b1c53befb03
wjl11011 <184313947@qq.com>
2022-07-23 15:01:24 +0800

    mdn study

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,37 @@
+/*let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/firefox-icon.png') {
+      myImage.setAttribute('src', '/image/wjl.jpg');
+    } else {
+      myImage.setAttribute('src', '/image/wjl2.jpg');
+    }
+}*/
+
+let myButton = document.querySelector('button');
 let myHeading = document.querySelector('h1');
-myHeading.textContent = '程序员前端造梦指南!';
+
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '👏欢迎光临,For' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = '👏欢迎光临,For' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+ }
+ function setUserName() {
+  let myName = prompt('请输入你的名字。');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = '👏欢迎光临,For，' + myName;
+  }
+}
